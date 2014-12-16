79260709b06bf81eae9187d8507589f2d4130980
Cyriaque Skrapits <cysoun@gmail.com>
2014-12-16 11:54:14 +0100

    Added missing files

---
+++ src/com/hearc/thereminoid/Thereminoid.java
@@ -0,0 +1,16 @@
+package com.hearc.thereminoid;
+
+/**
+ * Thereminoid main class.<br/>
+ * Contains some general stuff.
+ * 
+ * @author Cyriaque Skrapits
+ * @author Eddy Strambini
+ */
+public class Thereminoid {
+	// Theremin configuration
+	public static final float MIN_FREQUENCY = 20.0f;
+	public static final float MAX_FREQUENCY = 20000.0f;
+	
+	public static boolean mute = false;
+}
